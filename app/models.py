9a8b4760737a54995a2706712c09626dd3200d63
Abudlvosid Abdulhayev <abdulvosid780@gmail.com>
2024-11-15 15:27:14 +0500

    first commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    
